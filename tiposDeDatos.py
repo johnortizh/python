855029444c90c8a329029bf4d31f4aca75519c1c
johnortizh <johnortizh@outlook.com>
2025-11-19 17:36:06 -0500

    ok

--- tiposDeDatos.py
+++ tiposDeDatos.py
@@ -1,9 +1,19 @@
 # Tipos de datos
 
-print (type(5)) #Int
-print (type("Texto")) #Str
-print (type(True)) #Boolean
-print (type(6.4)) #float
-print (type(5+1j)) #complex
+print(type(5))  # Int
+print(type("Texto"))  # Str
+print(type(True))  # Boolean
+print(type(6.4))  # float
+print(type(5+1j))  # complex
 
-print ("nuevo cambio")
+print("nuevo cambio")
+
+variable = "primer variable"
+segundaVariable = "segunda variable"
+print(variable, "+", segundaVariable)
+
+edad = 34
+peso = 70.5
+
+masaCorporal = peso / (1.75 ** 2)
+print("Masa corporal:", masaCorporal)
